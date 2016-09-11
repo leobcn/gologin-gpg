f9c177f6fdf9a2b7580beb5df47e3fa24a7fc2fe
zowhoey <zowhoey@users.noreply.github.com>
2016-09-11 14:19:31 +0100

    Added TLS support

--- main.go
+++ main.go
@@ -12,21 +12,31 @@ type Page struct {
 	GPG   string
 }
 
-var port, publickeyring, username, tmpl *string
+var port, publickeyring, username, tmpl, tlsCert, tlsKey *string
+var httpDisable, tls *bool
 
 func main() {
 	//assign flags
 	port = flag.String("port", "9090", "port number")
-	publickeyring = flag.String("public_key", "", "GPG public keyring")
+	publickeyring = flag.String("public_key", "pubring.gpg", "GPG public keyring")
 	username = flag.String("user", "username", "change username")
 	tmpl = flag.String("template", "login.tmpl", "change template location")
+	tls = flag.Bool("tls", false, "use TLS")
+	tlsCert = flag.String("cert", "cert.pem", "location of cert.pem file")
+	tlsKey = flag.String("key", "key.pem", "location of key.pem file")
 	flag.Parse()
 
-	//handle http requsts
+	//handle http requests
 	http.HandleFunc("/", login)
 	http.HandleFunc("/processlogin", processlogin)
-	err := http.ListenAndServe(":"+*port, nil)
-	check(err)
+
+	if *tls == true {
+		err := http.ListenAndServeTLS(":"+*port, *tlsCert, *tlsKey, nil)
+		check(err)
+	} else if *tls == false {
+		err := http.ListenAndServe(":"+*port, nil)
+		check(err)
+	}
 
 }
 
